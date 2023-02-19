4434e8d2b33e21d163865a8b41c559c2aa3e771c
h3007 <mytem20007@gmail.com>
2023-02-19 20:09:05 +0500

    Virtualized Lists with Faker, исправлен на новый пакет

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import colorData from "./color-data.json";
-import ColorList from "./ColorList.js";
-
-export default function App() {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
-}
